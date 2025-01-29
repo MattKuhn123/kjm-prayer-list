d97ef37db9d5acdb56796fecebc78bb1bf439b87
Kuhn, Matthew L <mlkuhn@tva.gov>
2025-01-29 15:28:02 -0500

    fix navigation, fix class

--- js/nav.js
+++ js/nav.js
@@ -5,7 +5,8 @@ window.addEventListener("load", async (ev) => {
     const a = document.createElement("a");
     a.setAttribute("href", link.href);
     a.text = link.text;
-    if (link.external) {
+    const isExternal = !link.href.startsWith(".");
+    if (isExternal) {
       a.setAttribute("target", "_blank");
       a.setAttribute("class", "external");
     } else {
@@ -15,6 +16,17 @@ window.addEventListener("load", async (ev) => {
         const html = await (await (await fetch(target)).blob()).text();
         const sidebarContent = document.querySelector("#sidebar-content");
         sidebarContent.innerHTML = html;
+        
+        const sidebarNavMenu = document.querySelector("#sidebar-nav-menu");
+        sidebarContent.querySelectorAll("h1[id], h2[id").forEach(header => {
+          const text = header.textContent;
+          const a = document.createElement("a");
+          a.setAttribute("href", "#"+header.id);
+          a.text = text;
+          const li = document.createElement("li");
+          li.appendChild(a)
+          sidebarNavMenu.appendChild(li);
+        })
       });
     }
 
